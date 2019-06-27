7811dc0dacac77da36d96d696ad9a1e536c83035
jake hanes <jhanesx@yahoo.com>
2019-06-27 00:49:55 -0700

    on click displays gifs and emptys divbox

--- javascript/gif.js
+++ javascript/gif.js
@@ -20,6 +20,7 @@ console.log("im ready")
     
     $(".starterButtons").on("click", function(){
         console.log("click");
+        $("#gifBox").empty();
         const character = $(this).attr("data-person");
         console.log(character);
         const queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
@@ -29,6 +30,17 @@ console.log("im ready")
             method: "GET"
           }).then(function(response){
               console.log(response);
+              const results = response.data;
+              for (let i = 0; i < results.length; i++) {
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    const rating = results[i].rating;
+                    const p = $("<p>").text("Rating: " + rating);
+                    const personImage = $("<img>");
+                    personImage.attr("src", results[i].images.fixed_height.url);
+                    $("#gifBox").append(p);
+                    $("#gifBox").append(personImage);
+                }
+              }
           });
     });
     
